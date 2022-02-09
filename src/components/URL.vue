463e6d2697220d2ef7e2f1c10604568b3c5e4b06
Yannis Künzler <y.kuenzler@icloud.com>
2022-02-09 10:09:55 +0100

    add options

--- src/components/URL.vue
+++ src/components/URL.vue
@@ -9,24 +9,39 @@
         @input="createQR()"
         placeholder="https://"
     />
-    <div>
-      <a :href=picture target="_blank">
-        <img
-            :src=picture
-             width="250px"
-             alt="QR-code picture"
-             v-if="Boolean(picture)"
-             id="qrCode"
-             title="Open in new tab"
-        >
-      </a>
+    <div class="lowerFlex">
+      <div class="options">
+        <form>
+          <input type="color" name="options" id="color" @change="addOptions($event)">
+          <label for="color">Farbe</label><br><br>
+          <input type="radio" name="options" id="png" @change="addOptions($event)">
+          <label for="png">PNG</label><br>
+          <input type="radio" name="options" id="jpg" @change="addOptions($event)">
+          <label for="jpg">JPG</label><br>
+          <input type="radio" name="options" id="svg" @change="addOptions($event)">
+          <label for="svg">SVG</label>
+        </form>
+      </div>
+      <div class="qr-code">
+        <a :href=picture target="_blank">
+          <img
+              :src=picture
+              width="250px"
+              alt="QR-code picture"
+              v-if="Boolean(picture)"
+              id="qrCode"
+              title="Open in new tab"
+          >
+        </a>
+      </div>
     </div>
+
 
   </div>
 </template>
 
 <script>
-  const api_url = "https://api.qrserver.com/v1/create-qr-code/?";
+  const api_url = "https://api.qrserver.com/v1/create-qr-code/?margin=0&";
   export default {
     name: 'urlToPic',
     data() {
@@ -71,6 +86,7 @@ h1 {
 
 #qrCode {
   cursor:pointer;
+  margin: 0;
 }
 
 input {
@@ -102,6 +118,22 @@ button:hover {
 
 img {
   align-items: center;
-  margin: 20px;
+}
+
+.qr-code {
+  width: 250px;
+  height: 250px;
+  background-color: grey;
+  margin-left: 20px;
+}
+
+.options {
+  margin-right: 20px;
+}
+
+.lowerFlex {
+  display: flex;
+  justify-content: center;
+  ;
 }
 </style>
